4bc98856880b6b8760d8473b8ba8a62c9d0882f7
Ben Brodovsky <brodovsky.ben@gmail.com>
2020-09-18 11:28:26 -0400

    Adding some small updates

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 //the library will be visually displayed as a bunch of cards or a table,
 //not sure yet
-let myLibrary = []
+// let myLibrary = []
+//
+// function Book(title, author, pages, haveRead) {
+//   this.title = title
+//   this.author = author
+//   this.pages = pages
+//   this.haveRead = haveRead
+//
+//   this.info = function(){
+//     return `${title} by ${author}, ${pages}, ${haveRead}`
+//   }
+// }
 
-function Book(title, author, pages, haveRead) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.haveRead = haveRead
+// function displayBooks(){
+//   for(Book:myLibrary){
+//
+//   }
+// }
 
-  this.info = function(){
-    return `${title} by ${author}, ${pages}, ${haveRead}`
-  }
-}
+//add button logic
+let addButton = document.getElementById("addBookButton")
+if(addButton.addEventListener)
+  addbutton.addEventListener("click", addBookToLibrary, false)
+else if(addButton.attachEvent)
+  addButton.attachEvent('onclick', addBookToLibrary)
 
-function displayBooks(){
-  for(Book:myLibrary){
-
-  }
-}
-
-function addBookToLibrary(){
-
-}
+// function addBookToLibrary(){
+//   alert("test")
+// }
